4ea85a1af3dacd3a87e4738e98a52f9609b06d05
Marcio Cruz <marciocadev@gmail.com>
2022-03-25 11:53:34 -0300

    feat: PUT and GET objects to S3

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-# replace this
+# security-share-s3-files
+
+
+## bucket
+exemplo: `https://github.com/aws-samples/serverless-patterns/blob/main/apigw-s3-cdk/cdk/lib/apigw-s3-cdk-stack.ts`
+
+
+### GET
+```
+curl --location --request GET 'https://{restId}.execute-api.us-east-1.amazonaws.com/prod/assets/files/README.md'
+```
+
+#### no postman coloque na URL
+`'https://{restId}.execute-api.us-east-1.amazonaws.com/prod/assets/:folder/:key'`
+
+  * folder = files
+  * key = README.md
+
+
+### PUT
+```
+curl --location --request PUT 'https://ikekvxm1xg.execute-api.us-east-1.amazonaws.com/prod/assets/files/README.md' \
+--header 'Content-Type: text/markdown' \
+--data-binary '@/C:/Dev/smartbank/onboarding/coresignup-stack/README.md'
+```
+
+#### no postman coloque na URL
+`'https://{restId}.execute-api.us-east-1.amazonaws.com/prod/assets/:folder/:key'`
+
+  * folder = files
+  * key = README.md
+
+e em body, selecione `binary` e anexe o arquivo
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,133 @@
-import { App, Stack, StackProps } from 'aws-cdk-lib';
+import { App, RemovalPolicy, Stack, StackProps } from 'aws-cdk-lib';
+import { AwsIntegration, RestApi } from 'aws-cdk-lib/aws-apigateway';
+import { PolicyStatement, Role, ServicePrincipal } from 'aws-cdk-lib/aws-iam';
+import { Bucket, IBucket } from 'aws-cdk-lib/aws-s3';
 import { Construct } from 'constructs';
 
 export class MyStack extends Stack {
   constructor(scope: Construct, id: string, props: StackProps = {}) {
     super(scope, id, props);
 
-    // define resources here...
+    const securityBucket = new Bucket(this, 'SecurityBucket', {
+      bucketName: 'security-bucket-access-with-cdk',
+      publicReadAccess: false,
+      autoDeleteObjects: true,
+      removalPolicy: RemovalPolicy.DESTROY,
+    });
+
+    const bucketRest = new RestApi(this, 'BucketRest', {
+      restApiName: 'security-share-s3-files',
+      // field that enables API Gateway to handle binary media types.
+      // If the API needs to serve anything other than a text payload, this is required.
+      // binaryMediaTypes: ['application/octet-stream', 'image/jpeg'],
+      binaryMediaTypes: ['*/*'],
+      //  a field that enables compression of your content and specifies
+      // the minimum size of payload that has to be compressed
+      minimumCompressionSize: 0,
+    });
+
+    const resourceFolder = bucketRest.root.addResource('assets').addResource('{folder}');
+    const resourceKey = resourceFolder.addResource('{key}');
+    resourceKey.addMethod('GET', this.getFileIntegration(securityBucket), {
+      methodResponses: [
+        {
+          statusCode: '200',
+          responseParameters: {
+            'method.response.header.Content-Type': true,
+          },
+        },
+      ],
+      requestParameters: {
+        'method.request.path.folder': true,
+        'method.request.path.key': true,
+        'method.request.header.Content-Type': true,
+      },
+    });
+    resourceKey.addMethod('PUT', this.putFileIntegration(securityBucket), {
+      methodResponses: [
+        {
+          statusCode: '200',
+          responseParameters: {
+            'method.response.header.Content-Type': true,
+          },
+        },
+      ],
+      requestParameters: {
+        'method.request.path.folder': true,
+        'method.request.path.key': true,
+        'method.request.header.Content-Type': true,
+      },
+    });
+  }
+
+  // Put Object
+  private putFileIntegration(bucket: IBucket) {
+    const putRole = new Role(this, 'BucketPutRole', {
+      assumedBy: new ServicePrincipal('apigateway.amazonaws.com'),
+      roleName: 'bucket-put-role',
+    });
+    putRole.addToPolicy(new PolicyStatement({
+      resources: [bucket.bucketArn],
+      actions: ['s3:PutObject'],
+    }));
+    bucket.grantWrite(putRole);
+
+    return new AwsIntegration({
+      service: 's3',
+      integrationHttpMethod: 'PUT',
+      path: `${bucket.bucketName}/{folder}/{key}`,
+      options: {
+        credentialsRole: putRole,
+        integrationResponses: [
+          {
+            statusCode: '200',
+            responseParameters: {
+              'method.response.header.Content-Type': 'integration.response.header.Content-Type',
+            },
+          },
+        ],
+        requestParameters: {
+          'integration.request.path.folder': 'method.request.path.folder',
+          'integration.request.path.key': 'method.request.path.key',
+          // 'integration.request.header.Accept': 'method.request.header.Accept',
+        },
+      },
+    });
+  }
+
+  // GetObject Method
+  private getFileIntegration(bucket: IBucket) {
+    const getRole = new Role(this, 'BucketGetRole', {
+      assumedBy: new ServicePrincipal('apigateway.amazonaws.com'),
+      roleName: 'bucket-get-role',
+    });
+    getRole.addToPolicy(new PolicyStatement({
+      resources: [bucket.bucketArn],
+      actions: ['s3:GetObject'],
+    }));
+    bucket.grantRead(getRole);
+
+    return new AwsIntegration({
+      service: 's3',
+      integrationHttpMethod: 'GET',
+      path: `${bucket.bucketName}/{folder}/{key}`,
+      options: {
+        credentialsRole: getRole,
+        integrationResponses: [
+          {
+            statusCode: '200',
+            responseParameters: {
+              'method.response.header.Content-Type': 'integration.response.header.Content-Type',
+            },
+          },
+        ],
+        requestParameters: {
+          'integration.request.path.folder': 'method.request.path.folder',
+          'integration.request.path.key': 'method.request.path.key',
+          // 'integration.request.header.Accept': 'method.request.header.Accept',
+        },
+      },
+    });
   }
 }
 
